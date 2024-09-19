005aeef8378c58e9cd3ccef724f71dd08e81d602
admin-cmd <131160982+admincmd-a@users.noreply.github.com>
2024-09-19 23:16:15 +0800

    Update 0x00.js

--- aplayer/js/0x00.js
+++ aplayer/js/0x00.js
@@ -1,16 +1,6 @@
 const ap = new APlayer({
     container: document.getElementById('player'),
-    mini: false,
-    autoplay: false,
-    theme: '#FADFA3',
-    loop: 'all',
-    order: 'random',
-    preload: 'auto',
-    volume: 0.7,
-    mutex: true,
-    listFolded: false,
-    listMaxHeight: 90,
-    lrcType: 3,
+    fixed: true,
     audio: [
     {
         name: 'Grow Slowly',
